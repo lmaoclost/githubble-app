66fbeb37de4ad27f67bee6386818f59c70476ddc
lmaoclost <renansmoliveira@gmail.com>
2024-10-02 14:40:42 -0300

    fix: remove unused imports

--- src/components/ImageModal/index.tsx
+++ src/components/ImageModal/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Image, Modal, TouchableOpacity, TouchableWithoutFeedback, StyleSheet } from 'react-native';
+import { View, Image, Modal, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
 import { X } from 'lucide-react-native';
 import { styles } from './style'
 
@@ -19,20 +19,18 @@ export function ImageModal({ visible, onClose, imageUrl }: ImageModalProps) {
     >
       <TouchableWithoutFeedback onPress={onClose}>
         <View style={styles.modalContainer}>
-          <TouchableWithoutFeedback>
-            <View style={styles.modalContent}>
-              <TouchableOpacity
-                style={styles.closeButton}
-                onPress={onClose}
-              >
-                <X size={24} color="white" />
-              </TouchableOpacity>
-              <Image
-                style={styles.modalImage}
-                source={{ uri: imageUrl }}
-              />
-            </View>
-          </TouchableWithoutFeedback>
+          <View style={styles.modalContent}>
+            <TouchableOpacity
+              style={styles.closeButton}
+              onPress={onClose}
+            >
+              <X size={24} color="white" />
+            </TouchableOpacity>
+            <Image
+              style={styles.modalImage}
+              source={{ uri: imageUrl }}
+            />
+          </View>
         </View>
       </TouchableWithoutFeedback>
     </Modal>
